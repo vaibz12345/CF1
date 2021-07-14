6186cfa426b6893e9741d0b7cf5659ac30e8bf62
vaibz12345 <vaibhavsingh161997@gmail.com>
2021-07-14 16:25:34 +0530

    first Commmit

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,24 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+const btn=document.getElementById("btn");
+const color=document.querySelector(".color");
+
+
+btn.addEventListener("click", function()
+{
+ let hexcolour="#";
+
+ for(let i=0;i<6;i++)
+ {
+     hexcolour +=hex[getRandomNumber()];
+ }
+ color.textContent=hexcolour;
+ document.body.style.backgroundColor=hexcolour;
+});
+
+
+function getRandomNumber()
+{
+    return Math.floor(Math.random()*hex.length);
+}
